67871d517fbad46228f0341a3cfce03646ce30a6
JakariaMasud <jakariamasudliton@gmail.com>
2023-06-07 10:55:36 +0600

    13.Roman to Integer solved

---
+++ LeetCode/13. Roman to Integer.java
@@ -0,0 +1,28 @@
+public int romanToInt(String s) {
+        Map<Character,Integer> values = new HashMap<>();
+        int previousValue=0;
+        int sum=0;
+        values.put('I',1);
+        values.put('V',5);
+        values.put('X',10);
+        values.put('L',50);
+        values.put('C',100);
+        values.put('D',500);
+        values.put('M',1000);
+        for(int i=s.length()-1;i>=0;i--){
+            char singleChar = s.charAt(i);
+            int currentValue = values.get(singleChar);
+            if(currentValue>=previousValue ){
+               sum = sum+currentValue;
+                previousValue = currentValue;
+            }else{
+                sum = sum-currentValue;
+                previousValue = currentValue;
+            }
+
+        }
+
+    return sum;
+        
+    }
+
