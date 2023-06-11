014374badb18d0732f1836bcfbd1783a02d0551b
JakariaMasud <jakariamasudliton@gmail.com>
2023-06-11 18:52:38 +0600

    141.Linked List Cycle solved

---
+++ LeetCode/141.Linked List Cycle.java
@@ -0,0 +1,13 @@
+public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        Set<ListNode> nodeVisited = new HashSet<>();
+        while (head.next != null) {
+            if (nodeVisited.contains(head)) {
+                return true;
+            } else {
+                nodeVisited.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
