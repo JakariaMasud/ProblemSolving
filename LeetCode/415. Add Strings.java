defbfebc98d440c2c8d01ff16de4b4410d56e06e
JakariaMasud <jakariamasudliton@gmail.com>
2023-07-23 02:49:56 -0700

    415. Add Strings solved

---
+++ LeetCode/415. Add Strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+            StringBuilder sb = new StringBuilder();
+            int i = num1.length() - 1;
+            int j = num2.length() - 1;
+            int carry = 0;
+            while (i >= 0 || j >= 0) {
+                int sum = carry;
+                if (i >= 0) {
+                    int firstDigit = num1.charAt(i) - '0';
+                    sum += firstDigit;
+                    i--;
+                }
+                if (j >= 0) {
+                    int secondDigit = num2.charAt(j) - '0';
+                    sum += secondDigit;
+                    j--;
+                }
+                carry = sum / 10;
+                sb.append(sum % 10);
+    
+            }
+            if (carry > 0) sb.append(carry);
+            return sb.reverse().toString();
+        }
+    }
